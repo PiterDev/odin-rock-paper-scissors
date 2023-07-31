8785dbff9db7ef41611c722d73012a6d8d71679c
Piter <61157319+PiterDev@users.noreply.github.com>
2023-07-31 07:32:13 +0200

    Add game UI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
@@ -7,53 +10,63 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
     
     if (playerSelection === "rock" && computerSelection === "paper") {
+        showText("📃 beats 🪨.");
         return "computer";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        showText("📃 beats 🪨.");
         return "player";
     }
         
 
     if (playerSelection === "scissors" && computerSelection === "rock") {
+        showText("🪨 beats ✂️.");
         return "computer";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        showText("🪨 beats ✂️.");
         return "player";
     }
 
     if (playerSelection === "paper" && computerSelection === "scissors") {
+        showText("✂️ beat 📃.");
         return "computer";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        showText("✂️ beat 📃.");
         return "player";
     }
 
     if (playerSelection === computerSelection) {
+        showText("It's a draw!");
         return "draw";
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i<5; i++) {
-        let playerSelection = prompt("Pick rock/paper/scissors")
-        let result = playRound(playerSelection, getComputerChoice());
-        
-        
+document.addEventListener("DOMContentLoaded", () => {
+    let rock = document.querySelector(".rock");
+    let paper = document.querySelector(".paper");
+    let scissors = document.querySelector(".scissors");
+    let scoreDisplay = document.querySelector(".score-display");
+
+    [rock, paper, scissors].forEach(item => item.addEventListener("click", e => {
+        let result = playRound(item.className, getComputerChoice());
         if (result === "player") {
             playerScore++;
-            console.log("Player wins!");
         } else if (result === "computer") {
             computerScore++;
-            console.log("Computer wins!");
-        } else {
-            console.log("Draw!")
+        } 
+        scoreDisplay.innerText = `${playerScore} - ${computerScore}`;
+        if (playerScore >= 5) {
+            showText("Player wins!");
+            playerScore = 0;
+            computerScore = 0;
+        } else if (computerScore >= 5) {
+            showText("Computer wins!")
+            playerScore = 0;
+            computerScore = 0;
         }
-    }
+    }));
+})
 
-    if (playerScore > computerScore) {
-        console.log("You win!");
-    } else if (computerScore > playerScore) {
-        console.log("You lose :(")
-    } else {
-        console.log("It's a draw!")
-    }
+function showText(text) {
+    let resultText = document.querySelector(".result-text");
+    resultText.innerText = text;
 }
